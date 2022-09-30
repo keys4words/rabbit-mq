938324bdae93d436f1cf44da4ff8f584b58014e0
keys4words <keys4words@gmail.com>
2022-09-30 19:55:16 +0300

    start rabbit

---
+++ publisher/publisher.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	log "github.com/sirupsen/logrus"
+
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+var rabbit_host = os.Getenv("RABBIT_HOST")
+var rabbit_port = os.Getenv("RABBIT_PORT")
+var rabbit_user = os.Getenv("RABBIT_USERNAME")
+var rabbit_password = os.Getenv("RABBIT_PASSWORD")
+
+func main() {
+	router := httprouter.New()
+	router.POST("/publish/:message", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		submit(w, r, p)
+	})
+	fmt.Println("Running...")
+	log.Fatal(http.ListenAndServe(":80", router))
+}
+
+func submit(writer http.ResponseWriter, request *http.Request, p httprouter.Params) {
+	message := p.ByName("message")
+	fmt.Println("received message: " + message)
+	conn, err := amqp.Dial("amqp://" + rabbit_user + ":" + rabbit_password + "@" + rabbit_host + ":" + rabbit_port + "/")
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to connect to Rabbit", err)
+	}
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to open a channel", err)
+	}
+	defer ch.Close()
+	q, err := ch.QueueDeclare(
+		"publisher",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to declare a queue", err)
+	}
+
+	err = ch.Publish(
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to publish a message", err)
+	}
+	fmt.Println("publish successfully!")
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,28 @@
 # run a standalone instance
 docker network create rabbits
-docker run -d --rm --net rabbits --hostname rabbit-1 --name rabbit-1 rabbitmq:3.8 
+docker run -d --rm --net rabbits --hostname rab1 --name rab1 rabbitmq:3.11
+
+# gui
+localhost:8080
+guest/guest
+
+# inside container
+rabbitmqctl
+rabbitmq-plugins
 
 # how to grab existing erlang cookie
-docker exec -it rabbit-1 cat /var/lib/rabbitmq/.erlang.cookie
+docker exec -it rab1 cat /var/lib/rabbitmq/.erlang.cookie
+TZSWZEJJEPKTVXFMXPYRroot@rab1
+
+# plugins management
+docker run -d --rm --net rabbits -p 8080:15672 --hostname rab1 --name rab1 rabbitmq:3.11
+rabbitmq-plugins enable rabbitmq_management
+
+
+docker run -d --rm --net rabbits --hostname rab2 --name rab2 rabbitmq:3.11
+docker run -d --rm --net rabbits --hostname rab3 --name rab3 rabbitmq:3.11
+
+# publisher
+cd publisher
+docker build . -t keys4words/rabbit-pub:1.0
+docker run -it --rm --net rabbits -e RABBIT_HOST=rab1 -e RABBIT_PORT=5672 -e RABBIT_USERNAME=guest -e RABBIT_PASSWORD=guest -p 80:80 keys4words/rabbit-pub:1.0
